db7225850223e1861a25e1cb27dd32e37297e233
Jasmeetsaini801 <81513734+Jasmeetsaini801@users.noreply.github.com>
2021-06-19 10:56:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'CST Docker Rajinder'
-
-@app.route('/about')
-def hello_world():
-    return 'About Rajinder'
+    return 'CST Docker Home page'
 
 @app.route('/create')
 def hello_world():
-    return 'Create Rajinder'
+    return 'Creating Docker'
 
 if __name__== '__main__':
     app.run(host="0.0.0.0",debug=True)
